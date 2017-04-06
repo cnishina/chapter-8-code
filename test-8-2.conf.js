e25d9f2673ce35e9d7802cdaa3e01663dcb1a221
Craig Nishina <craig.nishina@gmail.com>
2017-04-06 10:01:50 -0700

    WIP BP

--- test-8-2.conf.js
+++ test-8-2.conf.js
@@ -1,10 +1,10 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 exports.config = {
+  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
   capabilities: {
     'browserName': 'chrome'
   },
-  directConnect: true,
   baseUrl: 'http://127.0.0.1:4200/',
   
   framework: 'jasmine',
